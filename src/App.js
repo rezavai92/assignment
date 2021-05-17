3fe55d064ac20735070b1d237ea5961afcde168f
rezavai92 <nijhumreza52@gmail.com>
2021-05-17 10:48:57 +0600

    baseui configured into root component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from   'react'
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider, styled} from 'baseui';
+import {StatefulInput} from 'baseui/input';
 
+const engine = new Styletron()
+function BaseUIWrapper ({children}){
+
+  return(<StyletronProvider value={engine} >
+     <BaseProvider theme={LightTheme} >{children}</BaseProvider>
+  </StyletronProvider>)
+}
 const App = ()=>{
 
 
   return(<div>
-    Hello 
+     <BaseUIWrapper>
+     </BaseUIWrapper>
   </div>)
 }
 
