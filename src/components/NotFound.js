8443089ca09ae4826d7b6e6f4aa1419b60ae8feb
rezavai92 <nijhumreza52@gmail.com>
2021-05-17 23:41:29 +0600

    final commit

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function NotFound() {
-    return (
-        <div>
-           404, Page Not Found
-        </div>
-    )
+  return <div>404, Page Not Found ...</div>;
 }
